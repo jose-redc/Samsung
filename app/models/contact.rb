593f14fdbe3ef069d8a9f344975e879bb275387a
jose <jose_redc@hotmail.com>
2019-07-18 03:29:30 +0000

    added code comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
+  
+  #Contact form validations 
   validates :name, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
