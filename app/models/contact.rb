97d1162d0f240e1388dfecaa201bfbde26cb4b60
jose <jose_redc@hotmail.com>
2019-07-18 02:06:52 +0000

    added email validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: {with: /.*@*./}
   validates :comments, presence: true
 end
