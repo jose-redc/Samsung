628153077247e35646a3c73396f3e279464ea6fa
jose <jose_redc@hotmail.com>
2019-08-07 05:06:06 +0000

    added css and additional link paths

--- app/controllers/lite_controller.rb
+++ app/controllers/lite_controller.rb
@@ -1,15 +1,3 @@
 class LiteController < ApplicationController
-    
-    def new
-        @lite = Lite.new
-        
-    end
-
-    def create
-        @lite = Lite.new(params[:lite]) 
-        if @lite.save
-            redirect_to lite_path
-        end
-    end    
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'lite', to: 'pages#lite'
+  get 'premium', to: 'pages#premium'
+  get 'rm', to: 'pages#rm'
+  get 'datalink', to: 'pages#datalink'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
